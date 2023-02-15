4a31f08d17e4d85db7c62937635dc2c2bd9e4920
Levon <levon90virabyan@gmail.com>
2023-02-15 22:59:47 +0400

    first commit

---
+++ main16.js
@@ -0,0 +1,29 @@
+const sentence = ["angel ","car","glean","arc "];
+let sente1 = "angel ";
+let sente2 = "glean";
+ let sente3 = "car";
+ let sente4 = "arc ";
+if(sente1.split().sort===sente2.split().sort()===sente3.split().sort===sente4.split().sort()){
+
+};
+console.log(sentence);
+// 
+const sentence1 = ["brag ","robed","grab","bored "];
+let sente5 = "brag ";
+let sente6 = "grab";
+ let sente7 = "robed";
+ let sente8 = "bored ";
+if(sente5.split().sort===sente6.split().sort()===sente7.split().sort===sente8.split().sort()){
+
+};
+console.log(sentence1);
+// 
+const sentence2 = ["cat  ","cider ","act","cried "];
+let sente9 = "cat";
+let sente10 = "act";
+ let sente11 = "cider ";
+ let sente12= "cried ";
+if(sente9.split().sort===sente10.split().sort()===sente11.split().sort===sente12.split().sort()){
+
+};
+console.log(sentence2);
